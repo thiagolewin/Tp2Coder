e3e116fe9ab202b958e1c4c8cf5e456ddf2ca0c8
thiagolewin <thiagolewin06@gmail.com>
2022-10-15 17:01:30 -0300

    Version 2.0 con galeria incluida

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const header = document.getElementsByTagName("header")[0];
 const body = header.parentElement;
 const h1 = header.children[2];
 const article = document.getElementsByTagName("article");
+const footer = document.getElementsByTagName("footer")[0];
+
 h2Black = document.getElementsByTagName("h2");
 
 hamburgesa.addEventListener(`click`, ()=> {
     hamburgesa.classList.toggle('rotate');
     menu.classList.toggle('visible');
     header.classList.toggle('negro');
+    footer.classList.toggle('negro');
+    footer.classList.toggle('blancoTexto')
     h1.classList.toggle('margen');
     body.classList.toggle('blanco');
     if (h1.style.color != "white") {
@@ -18,12 +22,14 @@ hamburgesa.addEventListener(`click`, ()=> {
         h2Black[0].style.color = "black";
         h2Black[1].style.color = "black";
         h2Black[2].style.color = "black";
+        footer.style.color = "white";
     } else {
         h1.style.color = "black"
         hamburgesa.style.color = "black"
         h2Black[0].style.color = "white";
         h2Black[1].style.color = "white";
         h2Black[2].style.color = "white";
+        footer.style.color = "blackz";
     }
 })
 
